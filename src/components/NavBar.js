e28dcaf96966cbfc5731b2377cbe03b900c6d36d
yohannes2025 <johannes.m.tekle@gmail.com>
2025-09-06 12:50:10 +0200

    Commit : Add NavBar component Subject: Add navigation bar with auth links Body: Create NavBar.js with conditional links for logged-in/out states and username display for User Story 1 (LO4.7). This provides intuitive navigation and login state clarity.

---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+// src/components/NavBar.js
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NavBar = ({ isLoggedIn, onLogout, user }) => {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Productivity App
+        </Navbar.Brand>{" "}
+        {/* Corrected closing tag */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {isLoggedIn && user && (
+              <Nav.Link disabled>Welcome, {user.username}</Nav.Link>
+            )}
+            {isLoggedIn ? (
+              <>
+                <Nav.Link as={Link} to="/tasklist">
+                  Task List
+                </Nav.Link>
+                <Nav.Link as={Link} to="/createtask">
+                  Create Task
+                </Nav.Link>
+                <Nav.Link onClick={onLogout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
