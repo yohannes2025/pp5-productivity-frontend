97cec15c303ce43c2d66f44bf86ee68455359e28
yohannes2025 <johannes.m.tekle@gmail.com>
2025-09-08 01:34:15 +0200

    Commit: refactor: enhance NavBar with responsive design and styled links
    Subject:
    Refactor NavBar.js to improve responsiveness and UX
    
    Body:
    
    Replaced Link with NavLink for active route highlighting.
    
    Added useState and useEffect hooks to manage navbar collapse state on route change and link click.
    
    Applied custom styles from NavBar.module.css for consistent theming.
    
    Included app logo and updated branding inside the navbar.
    
    Improved navigation UX with icons (FontAwesome) and bold styling.
    
    Ensured the navbar closes automatically on navigation, enhancing mobile usability.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,47 +1,82 @@
-// src/components/NavBar.js
-import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+// export default NavBar;
 
-const NavBar = ({ isLoggedIn, onLogout, user }) => {
+import React, { useState, useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function NavBar({ isLoggedIn, onLogout }) {
+  const [expanded, setExpanded] = useState(false); // manages collapse state
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpanded(false); // close navbar on route change
+  }, [location.pathname]);
+
+  const handleNavClick = () => {
+    setExpanded(false); // close navbar on link click
+  };
+
   return (
-    <Navbar expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          Productivity App
-        </Navbar.Brand>{" "}
-        {/* Corrected closing tag */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            {isLoggedIn && user && (
-              <Nav.Link>Welcome, {user.username}</Nav.Link>
-            )}
-            {isLoggedIn ? (
-              <>
-                <Nav.Link as={Link} to="/tasklist">
-                  Task List
-                </Nav.Link>
-                <Nav.Link as={Link} to="/createtask">
-                  Create Task
-                </Nav.Link>
-                <Nav.Link onClick={onLogout}>Logout</Nav.Link>
-              </>
-            ) : (
-              <>
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to="/register">
-                  Register
-                </Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <div>
+        <Navbar
+          expand="md"
+          fixed="top"
+          expanded={expanded}
+          onToggle={setExpanded}
+          className={styles.NavBar}
+        >
+          <Container>
+            <Navbar.Brand className="me-auto">
+              <img src="/logo192.png" alt="logo" height="45" className="me-2" />
+              <span className="fw-bold">Productivity</span>
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav className="ms-auto my-2 my-lg-0" navbarScroll>
+                {!isLoggedIn && (
+                  <NavLink
+                    to="/"
+                    className={`${styles.NavLink} fw-bold`}
+                    onClick={handleNavClick}
+                  >
+                    <i className="fa-solid fa-house"></i> Home Page
+                  </NavLink>
+                )}
+                {isLoggedIn && (
+                  <>
+                    <NavLink
+                      to="/CreateTask"
+                      className={`${styles.NavLink} fw-bold`}
+                      onClick={handleNavClick}
+                    >
+                      <i className="fa-solid fa-square-plus"></i> Create Task
+                    </NavLink>
+                    <NavLink
+                      to="/tasklist"
+                      className={`${styles.NavLink} fw-bold`}
+                      onClick={handleNavClick}
+                    >
+                      <i className="fa-solid fa-list-check"></i> Task List
+                    </NavLink>
+                    <Nav.Link
+                      onClick={onLogout}
+                      className={`${styles.NavLink} fw-bold`}
+                    >
+                      <i className="fa-solid fa-sign-out-alt"></i> Logout
+                    </Nav.Link>
+                  </>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </>
   );
-};
+}
 
 export default NavBar;
