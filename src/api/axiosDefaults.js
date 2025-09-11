2d6a7bdb29a172f5af15cbf684d6f505f3d41193
yohannes2025 <johannes.m.tekle@gmail.com>
2025-09-11 21:05:48 +0200

    Update API baseURL to https://pp5-productivity-backend.onrender.com in axiosDefaults.js and handlers.js

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "pp5-productivity-backend.onrender.com";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
